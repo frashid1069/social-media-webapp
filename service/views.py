17a6f339e3c687f0ce5e707b77247f1d0128c27b
AnshD8619 <ansh.desai86@gmail.com>
2024-10-11 15:51:49 -0600

    Created backend functions for many functions. Need to fill in the return statements with the proper ui redirects with the correct information being passed through.

--- service/views.py
+++ service/views.py
@@ -34,7 +34,11 @@ class InboxView(ModelViewSet):
     queryset = models.Inbox.objects
     serializer_class = serializers.LikeSerializer
 
+"""
+Creates a post and saves it in the database
+"""
 def create_post(request):
+    # Checks if request is a POST, then gets information and creates the post
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
@@ -43,15 +47,63 @@ def create_post(request):
         visibility = request.POST.get("visibility")
         author = request.POST.get("author")
         models.Post.objects.create(title=title, content=content, content_type = content_type, image_content = image_content, visbility=visibility, author=author)
+    # Need to return to the ui page
     return
 
-def create_comment(request, post_id):
+"""
+Creates comment for the post and saves it in the database
+"""
+def create_comment(request, post_id, author_id):
+    if request.method == "POST":
+        content = request.POST.get("content")
+        post = models.Post.objects.get(id=post_id)
+        author = models.Author.objects.get(id=author_id)
+        if content == None or len(content) == 0:
+            # Error return to page again, does not create comment
+            return 
+        # Creates comment
+        models.Comment.objects.create(content=content, post=post, author=author_id)
+    # Redirect to page here, comment is added now
     return
 
-def delete_post(request):
+def delete_post(request, post_id):
+    # Request should send through post id
+    # From https://stackoverflow.com/questions/3805958/how-to-delete-a-record-in-django-models by Wolph
+    models.Post.objects.filter(id=post_id).delete()
+    # Return to ui
     return
 
 def get_public_posts(request, author_id):
-    posts = models.Post.filter(authot=author_id, visibility="public")
-    
+    posts = models.Post.filter(id=author_id, visibility="public")
+    # Return to the ui page, pass through the list of posts above
+    return
+
+def edit_post(request, post_id):
+    post = models.Post.objects.get(id=post_id)
+    serializer = serializers.Post(post, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    # Return to ui
+    return 
+
+def follow_author(request, author_id):
+    follower = models.Author.objects.get(id=author_id)
+    followed = request.POST.get("followed")
+    model.Follow.objects.create(follower=follower, followed=followed)
+    # Return to page now
+    return
+
+def handle_follow(request, follow_id):
+    if request.get("choice") == "no":
+        # From https://stackoverflow.com/questions/3805958/how-to-delete-a-record-in-django-models by Wolph
+        models.Follow.objects.filter(id=follow_id).delete()
+    else:
+        follow = models.Follow.get(id=follow_id)
+        follow["pending"] = "no"
+        follow.save()
+    # Return to ui
+    return
+
+def get_stream_posts(request):
     return

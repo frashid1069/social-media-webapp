e29319e874d4f794d52577dffa7de12ffa93737e
Farhan Rashid <frashid1069@gmail.com>
2024-10-12 15:38:24 -0600

    added functionality for editing posts

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-import './EditPost.css';
+import '../editPost.css';
 
 const EditPost = () => {
     const { postId } = useParams(); // Extract postId from URL

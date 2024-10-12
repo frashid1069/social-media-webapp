57851a6af378d19a1ccc123fb2e2e3e5244a3a6a
Yoochu <rchu3@ualberta.ca>
2024-10-12 14:43:36 -0600

    update login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
+import SignUp from "./components/SignUp";
+import Stream from "./components/Stream";
 
 function App() {
   return (
     <div className="App">
       <div className="login">
-        <Login></Login>
+        <Router basename="/">
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/stream" element={<Stream />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>

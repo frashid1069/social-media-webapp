4347484623a2ef926c99c556ea30b7e1c39fa3a9
Farhan Rashid <frashid1069@gmail.com>
2024-10-12 15:27:40 -0600

    added functionality for editing posts

--- service/views.py
+++ service/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 from . import serializers, models
 
 # Later on, the index function will be used to handle incoming requests to polls/ and it will return the hello world string shown below.
@@ -17,6 +18,25 @@ class AuthorView(ModelViewSet):
 class PostView(ModelViewSet):
     queryset = models.Post.objects
     serializer_class = serializers.PostSerializer
+
+    @action(detail=True, methods=['put'], url_path='edit')
+    def edit_post(self, request, pk=None):
+        post = self.get_object()  # Get the post object based on the provided pk
+
+        if post.author != request.user:
+            return Response({'Error': 'You are not allowed to edit this post.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        title = request.data.get('title', post.title)  # Get new title if provided
+        content = request.data.get('content', post.content)  # Get new content if provided
+        
+        # Update the post fields
+        serializer = self.get_serializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 class CommentView(ModelViewSet):
     queryset = models.Comment.objects

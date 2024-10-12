921b57ca7ac0a9f412eb982d578d6df93ea5a48a
Farhan Rashid <frashid1069@gmail.com>
2024-10-12 15:13:08 -0600

    added functionality for: other authors cannot modify my posts

--- service/views.py
+++ service/views.py
@@ -20,25 +20,6 @@ class PostView(ModelViewSet):
     queryset = models.Post.objects
     serializer_class = serializers.PostSerializer
 
-    @action(detail=True, methods=['put'], url_path='edit')
-    def edit_post(self, request, pk=None):
-        post = self.get_object()  # Get the post object based on the provided pk
-
-        if post.author != request.user:
-            return Response({'Error': 'You are not allowed to edit this post.'}, status=status.HTTP_403_FORBIDDEN)
-        
-        title = request.data.get('title', post.title)  # Get new title if provided
-        content = request.data.get('content', post.content)  # Get new content if provided
-        
-        # Update the post fields
-        serializer = self.get_serializer(post, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-
 class CommentView(ModelViewSet):
     queryset = models.Comment.objects
     serializer_class = serializers.CommentSerializer

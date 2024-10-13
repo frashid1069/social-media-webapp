7d49153fad48fb391dabc98d0a0bf7463e9f3fb6
Yoochu <90581086+Yoochu@users.noreply.github.com>
2024-10-13 13:08:02 -0600

    done with Login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../loginStyles.css";
-// import SignUp from "./SignUp";
-import Stream from "./Stream";
 
 export default function Login() {
   const [authors, setAuthors] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
   // get the author list
   useEffect(() => {
     fetch("http://localhost:8000/service/author/")
@@ -22,19 +21,23 @@ export default function Login() {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
   // check if the input username and author's username match
   const matchesUsername = (author, username) => {
     return author.username === username;
   };
+
   // check if the input password and author's password match
   const matchesPassword = (author, password) => {
     return author.password === password;
   };
+
   // find the matched author by comparing username and password
   const filterAuthor = authors.filter(
     (author) =>
       matchesUsername(author, username) && matchesPassword(author, password)
   );
+
   // for existed user, redirect to its stream page, otherwise redirect to sign up page
   function verify() {
     if (filterAuthor.length === 0) {
@@ -43,10 +46,15 @@ export default function Login() {
       navigate(`/stream/${filterAuthor[0].id}`);
     }
   }
+  function goSignUp() {
+    navigate("/signup");
+  }
   return (
     <div className="login-page">
-      <h2 className="app-subtitle">Welcome to the login page!</h2>
+      <h2 className="page-subtitle">Welcome to the login page!</h2>
+
       <img className="login-image" src="login-image.png" alt="login" />
+
       <input
         type="text"
         className="username"
@@ -54,6 +62,7 @@ export default function Login() {
         value={username}
         onChange={handleUsernameChange}
       />
+
       <input
         type="password"
         className="password"
@@ -65,11 +74,10 @@ export default function Login() {
       <button type="submit" className="login-button" onClick={verify}>
         Login
       </button>
-      <Link to="/signup">
-        <button type="submit" className="signup-button">
-          Sign Up
-        </button>
-      </Link>
+
+      <button type="submit" className="signup-button" onClick={goSignUp}>
+        Sign Up
+      </button>
     </div>
   );
 }

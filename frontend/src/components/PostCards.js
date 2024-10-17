fc9da97da7b86a0889e0f4b8c58c104339776c81
Yoochu <90581086+Yoochu@users.noreply.github.com>
2024-10-17 04:58:05 -0600

    Add profile

---
+++ frontend/src/components/PostCards.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import "../streamStyle.css";
+import Comment from "./Comment";
+
+/**
+ * This is a component for displaying posts for the corresponded author using Comment component
+ * @param post: single post object
+ * @param editable: a boolean that implies whether the post is editable
+ * Click author button => go to the corresponding author's profile
+ */
+
+export default function PostCards({ post, editable }) {
+  const [authors, setAuthors] = useState([]);
+  const [comments, setComments] = useState([]);
+  const navigate = useNavigate();
+  // get the author list
+  useEffect(() => {
+    fetch("http://localhost:8000/service/author/")
+      .then((response) => response.json())
+      .then((data) => setAuthors(data));
+  }, []);
+  // get the comment list
+  useEffect(() => {
+    fetch("http://localhost:8000/service/comment/")
+      .then((response) => response.json())
+      .then((data) => setComments(data));
+  }, []);
+  // find the corresponding author's name for the post
+  const matchAuthor = (authorId) => {
+    for (const author of authors) {
+      if (author.id === authorId) {
+        return author.display_name;
+      }
+    }
+  };
+  // find the comments for the post
+  const commentFilter = (comment) => {
+    return comment.post === post.id;
+  };
+  const matchedComments = comments.filter((comment) => commentFilter(comment));
+  // go to edit page if
+  const goEdit = () => {
+    if (editable) {
+      navigate(`/stream/${post.author}/${post.id}/edit`);
+    }
+  };
+
+  // go to the corresponding profile
+  const goProfile = () => {
+    navigate(`/stream/${post.author}/profile`);
+  };
+
+  return (
+    <div key={post.id} className="post-card" onClick={goEdit}>
+      <h3 className="post-card-title">{post.title}</h3>
+      <button className="post-card-author" onClick={goProfile}>
+        {matchAuthor(post.author)}
+      </button>
+      <p className="post-card-content">{post.content}</p>
+      <p className="post-card-update-date">Updated at: {post.updated_at}</p>
+      <div className="comment-grid">
+        <h5 className="comment-title">Comments: </h5>
+        {matchedComments.map((comment) => (
+          <Comment comment={comment} key={comment.id}></Comment>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import "../streamStyle.css";
+import PostCards from "./PostCards";
+
+/**
+ * This is a component for displaying the profile page
+ *
+ *
+ */
+export default function Profile() {
+  const [author, setAuthor] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const { authorId } = useParams();
+  // get the author info
+  useEffect(() => {
+    fetch(`http://localhost:8000/service/author/${authorId}`)
+      .then((response) => response.json())
+      .then((data) => setAuthor(data));
+  }, [authorId]);
+  // get the posts list
+  useEffect(() => {
+    fetch("http://localhost:8000/service/post/")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+  // get the author id as an int
+  const authorIdInt = parseInt(authorId);
+
+  // check if the post is from current user
+  const matchesAuthor = (post, id) => {
+    return post.author === id;
+  };
+  // get posts that belong to the current user
+  const visiblePosts = posts.filter((post) => matchesAuthor(post, authorIdInt));
+  // sort visible posts so that the most recent updated posts appear at the top
+  const sortedPosts = visiblePosts
+    .sort((a, b) => {
+      return (
+        new Date(a.scheduled_for).getTime() -
+        new Date(b.scheduled_for).getTime()
+      );
+    })
+    .reverse();
+  return (
+    <div className="profile-page">
+      <h2 className="page-subtitle">Welcome to the Profile page!</h2>
+      <h4 className="profile-txt">Name: </h4>
+      <p className="profile-name">{author.display_name}</p>
+      <h4 className="profile-txt">Bio: </h4>
+      <p className="profile-bio">{author.bio}</p>
+      <h4 className="profile-txt">Github URL: </h4>
+      <p className="profile-git">{author.github_url}</p>
+      <div className="post-grid">
+        {sortedPosts.map((post) => (
+          <PostCards post={post} key={post.id} editable={false}></PostCards>
+        ))}
+      </div>
+    </div>
+  );
+}

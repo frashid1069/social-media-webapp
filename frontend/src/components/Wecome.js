304f9c07491da4b38ea752d65554660b1cb8ae1a
Yoochu <90581086+Yoochu@users.noreply.github.com>
2024-10-17 20:56:06 -0600

    Welcome page added

---
+++ frontend/src/components/Wecome.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../loginStyles.css";
+import { useNavigate } from "react-router-dom";
+/**
+ * This a component for displaying the welcome page
+ *
+ *
+ */
+export default function Welcome() {
+  const navigate = useNavigate();
+  const goLogin = () => {
+    navigate("/login");
+  };
+  const goSignup = () => {
+    navigate("/signup");
+  };
+  return (
+    <div>
+      <h1>Welcome to Aquamarine 404 Project</h1>
+      <button className="go-to-login" onClick={goLogin}>
+        Go Login
+      </button>
+      <button className="go-to-signup" onClick={goSignup}>
+        Go Sign Up
+      </button>
+    </div>
+  );
+}

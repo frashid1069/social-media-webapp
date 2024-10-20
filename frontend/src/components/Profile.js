b4717eca7d55aeb4f8952e508065ea3d8d7f4fa5
Farhan Rashid <frashid1069@gmail.com>
2024-10-20 13:34:24 -0600

    Update Profile.js with userNavigate

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -12,6 +12,7 @@ export default function Profile() {
   const [author, setAuthor] = useState([]);
   const [posts, setPosts] = useState([]);
   const { authorId } = useParams();
+  const navigate = useNavigate();
   // get the author info
   useEffect(() => {
     fetch(`http://localhost:8000/service/author/${authorId}`)

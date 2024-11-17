0c6c31c52d3690d031672be9ed6afdcd34b3b723
Rex <yongxin1@ualberta.ca>
2024-11-17 12:51:28 -0700

    heroku setting

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn aquamarine_server.wsgi
+web: cd frontend && npm install && npm run build && cd .. && gunicorn aquamarine_server.wsgi
--- aquamarine_server/settings.py
+++ aquamarine_server/settings.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'service.authentication.JwtQueryParamsAuthentication',  # authentication
+        # TODO: basic authentication
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',

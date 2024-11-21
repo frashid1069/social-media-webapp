479f75c36e29aef0263d2e159be61d74a74d7c6a
Farhan Rashid <frashid1069@gmail.com>
2024-11-21 01:10:28 -0700

    follow request test

--- frontend/src/components/AuthorsPage.js
+++ frontend/src/components/AuthorsPage.js
@@ -62,7 +62,9 @@ export default function AuthorsPage() {
         const actor = await actorResponse.json();
       
         // Fetch the author to follow's details
+        // const objectResponse = await cusFetch(`${apiUrl}forward/${apiUrl}authors/${authorID}/`, {
         const objectResponse = await cusFetch(`${apiUrl}authors/${authorID}/`, {
+
           method: "GET",
           headers: {
             token: `${token}`,
@@ -102,7 +104,7 @@ export default function AuthorsPage() {
         });
       
         if (response.ok) {
-          alert(`You have followed this author`);
+          alert(`You have sent a follow request to this author`);
           setIsFollowing((prev) => ({ ...prev, [authorID]: true })); // Mark this author as followed
         } else {
           alert("Failed to follow the author.");

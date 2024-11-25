307814e422568fe498d6cf18b2db83c65476434c
Rex <yongxin1@ualberta.ca>
2024-11-25 04:07:44 -0700

    single post fqid fixed

--- post/urls.py
+++ post/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # post api
     path('authors/<int:AUTHOR_SERIAL>/posts/', views.post_list, name='post_list'),
     path('authors/<int:AUTHOR_SERIAL>/posts/<int:POST_SERIAL>', views.post_detail, name='post_detail'),
-    path('authors/posts/<path:POST_FQID>', views.fqid_post_detail, name='fqid_post_detail'),
+    path('posts/<path:POST_FQID>', views.fqid_post_detail, name='fqid_post_detail'),
     # image post api
     path('authors/<int:AUTHOR_SERIAL>/posts/<int:POST_SERIAL>/image', views.post_image, name='post_image'),
     path('posts/<path:POST_FQID>/image', views.post_image, name='fqid_post_image'),
